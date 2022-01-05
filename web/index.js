86fd11fe72bd6d3d19b288b334f3e164085de612
Sayan-code-das <86365904+Sayan-code-das@users.noreply.github.com>
2022-01-05 12:30:39 +0530

    Add files via upload

---
+++ web/index.js
@@ -0,0 +1,12 @@
+
+ var a = document.getElementById("hide");
+ var b=document.getElementsByClassName("main")
+ function my(){
+     if(a.style.display=="block"){
+         a.style.display="none";
+         
+     }
+     else{
+         a.style.display="block"
+     }
+ }
